1a61eb05fd6414bcc9db33df47bcd736a727d65f
Kirmada <64690540+Kirmada7@users.noreply.github.com>
2020-07-28 21:26:22 +0530

    check

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ async def on_ready():
 async def on_member_join(member):
     print(f'{member} has joined the server')
 
-
+#disc
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left the server')
